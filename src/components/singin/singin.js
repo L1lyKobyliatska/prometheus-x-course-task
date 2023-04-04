2be619c6baa93683aba8bb405d1355f7459e6726
Liliia Kobyliatska <liliyadanko2311@icloud.com>
2023-04-04 15:59:22 +0300

    +links

--- src/components/singin/singin.js
+++ src/components/singin/singin.js
@@ -34,7 +34,7 @@ export default function Signin() {
     }, [username]);
 
     if (localStorage.authorization) {
-        document.location = 'books-list';
+        document.location = 'prometheus-x-course-task/books-list';
     } else {
         return (
             <main>
